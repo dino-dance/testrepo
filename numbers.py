7782ea9e5ca0a3d3965c2fe281b08f568a211c94
dino-dance <71885099+dino-dance@users.noreply.github.com>
2020-09-25 09:13:05 -0400

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,14 @@
+
+# Existen dos tipos de datos, enteros y decimales
+
+# print(type(10))
+# print(type(10.4))
+
+# print(1+1)
+
+#Puedo hacer cualquier operacion con numeros utilizando los argumentos matematicos
+
+
+age = input("Insert your age: ")
+new_age = int(age) + 5
+print(new_age)
